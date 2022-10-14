562e04dcb2a523c487967b7f052d0ad23b767da8
Jonas Graubner <jogithub@graubner-bayern.de>
2022-10-14 12:38:31 +0200

    Create DBConnect.php

---
+++ SOURCE/Server/DBConnect.php
@@ -0,0 +1,13 @@
+<?php
+$client = new MongoDB\Client(
+    'mongodb://192.168.0.40:27017/'
+);
+
+$collection = $client->test->test;
+
+$result = $collection->find();
+
+foreach ($result as $entry) {
+    print_r($entry);
+    print("<br>");
+}
