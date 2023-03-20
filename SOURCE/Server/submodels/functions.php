d9db459e1987a466647a0043cac02d711f602534
paulbrenner2 <96117377+paulbrenner2@users.noreply.github.com>
2023-03-20 17:43:36 +0100

    Update functions.php

--- SOURCE/Server/submodels/functions.php
+++ SOURCE/Server/submodels/functions.php
@@ -5,18 +5,19 @@ function deleteSubmodel($ShellIdent)
     http_response_code(501);
     exit;
 }
-    function getSubmodelIdFromAsset($assetRef){
-        $DBresult = readDB("Submodels",
-            ['assetInformation.globalAssetId.keys.value' => $assetRef],
-            ["projection" => ["id"=>1]]);
-        return $DBresult[0];
-    }
-    function GetAllSubmodels()
-    {
-        $DBresult = readDB("Submodels", array(), array());
-        array_walk($DBresult, "removeIDfromResult");
-        return $DBresult;
-    }
+function getSubmodelIdFromAsset($assetRef)
+{
+    $DBresult = readDB("Submodels",
+        ['assetInformation.globalAssetId.keys.value' => $assetRef],
+        ["projection" => ["id"=>1]]);
+    return $DBresult[0];
+}
+function GetAllSubmodels()
+{
+    $DBresult = readDB("Submodels", array(), array());
+    array_walk($DBresult, "removeIDfromResult");
+    return $DBresult;
+}
 
 function GetAllSubmodelsByIdShort($IdShort)
 {
