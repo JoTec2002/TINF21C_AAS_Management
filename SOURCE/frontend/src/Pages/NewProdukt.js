e1c99cdbde832f70573c640131143a30e1eed4be
Ayunda <SelvanaDwi.Ayunda@elk-wue.de>
2023-03-23 08:52:21 +0100

    neue Roter for neu produkt and new deteilsprodukt

---
+++ SOURCE/frontend/src/Pages/NewProdukt.js
@@ -0,0 +1,15 @@
+import {DetailsProdukt, Mydocs, NavComponent, Produkte} from "../components";
+import {Form, Button, Col,Row} from "react-bootstrap";
+
+const NewProdukt =()=>{
+    return(
+        <div>
+            <NavComponent/ >
+            <div class="newprodukt">
+    HULÖOÖO
+            </div>
+
+        </div>
+    )
+}
+export default NewProdukt
--- SOURCE/frontend/src/utils/RouterPages.js
+++ SOURCE/frontend/src/utils/RouterPages.js
@@ -3,6 +3,7 @@ import Guest from "../Pages/Guest";
 import  Home from "../Pages/Home";
 import AdminDashboard from "../Pages/AdminDashboard";
 import Notfound from "../Pages/Notfound";
+import NewProdukt from "../Pages/NewProdukt";
 const RouterPages =()=>{
     return (
         <Router>
@@ -10,6 +11,7 @@ const RouterPages =()=>{
                 <Route path='/' element={<Guest />}/>
                 <Route path='/home' element={<Home />}/>
                 <Route path='/admin' element={<AdminDashboard />}/>
+                <Route path='/new' element={<NewProdukt />}/>
                 <Route path='/*' element={<Notfound />}/>
             </Routes>
         </Router>
