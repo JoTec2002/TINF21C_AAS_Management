c93d2b3188206b2c09fedbb89eb141919f50c4ea
Ayunda <SelvanaDwi.Ayunda@elk-wue.de>
2023-04-17 14:48:27 +0200

    IDEndcoded and found pic for Norgen

--- SOURCE/frontend/src/components/DetailsProdukt.js
+++ SOURCE/frontend/src/components/DetailsProdukt.js
@@ -3,10 +3,12 @@ import axios from "axios";
 import { Col, Card, Row, Button, ListGroup } from "react-bootstrap";
 import { API_URL } from "../utils/constanst";
 import Spinner from 'react-bootstrap/Spinner';
+import { base64url } from "base64url";
 
 const DetailsProdukt = ({ data }) => {
   const [produktData, setProduktData] = useState(null);
   const [loading, setLoading] = useState(false);
+  const base64url = require('base64url');
 
   useEffect(() => {
     setLoading(true); // نمایش صفحه لودینگ
@@ -46,6 +48,14 @@ const DetailsProdukt = ({ data }) => {
       </Col>
     );
   }
+  produktData && produktData.map((shells) => (
+
+      // soll hier eine Iteration umzu suche welche submodelsID(produktData[0].submodels[3].keys[0].value)gultig ist
+
+      produktData[0].idEncoded= base64url.fromBase64(window.btoa(produktData[0].submodels[3].keys[0].value))
+  ))
+  // für Bild GeneralInformation.ProductImage01=idShortPath, darf nicht hartcoded
+
 
   return (
     <Col md={8} mt="2">
@@ -56,7 +66,8 @@ const DetailsProdukt = ({ data }) => {
       <Row>
         <Col md={6} xs={6}>
           <Card style={{ border: "none" }}>
-            <Card.Img variant="top" src="empty.png" />
+
+            <Card.Img variant="top" src={API_URL+'submodels/'+produktData[0].idEncoded+'/submodelelements/GeneralInformation.ProductImage01/attachment'} />
           </Card>
         </Col>
         <Col md={6} xs={6}>
@@ -81,6 +92,10 @@ const DetailsProdukt = ({ data }) => {
                 <ListGroup.Item className="fw-bold">
                   Id or Link:
                   <Card.Text className="fw-normal">{produktData[0].id}</Card.Text>
+                </ListGroup.Item>
+                <ListGroup.Item className="fw-bold">
+                  IdEncoded:
+                  <Card.Text className="fw-normal">{produktData[0].idEncoded}</Card.Text>
                 </ListGroup.Item>
               </ListGroup>
             </Card.Body>
