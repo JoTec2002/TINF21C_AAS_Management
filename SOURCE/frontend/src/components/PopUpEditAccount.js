9bd072d855c4f399746d6c26c009086f8505c867
Luka Pavic <luka.pavic@gmx.net>
2023-05-02 09:18:49 +0200

    Update for editing and deleting an account to server and error-handling.

---
+++ SOURCE/frontend/src/components/PopUpEditAccount.js
@@ -0,0 +1,84 @@
+import { Button, Form, Modal } from "react-bootstrap";
+import React from "react";
+import axios from "axios";
+import {API_URL} from "../utils/constanst";
+
+function handleEdit(accID){
+
+    console.log(`${API_URL}submodels/aHR0cHM6Ly9leGFtcGxlLmNvbS9pZHMvc20vMzM4MV80MTYwXzQwMzJfMzc1Mw/submodelelements/basicAuth.${accID}`);
+
+    axios.get(`${API_URL}submodels/aHR0cHM6Ly9leGFtcGxlLmNvbS9pZHMvc20vMzM4MV80MTYwXzQwMzJfMzc1Mw/submodelelements/basicAuth.${accID}`, {
+        auth: {
+            username: localStorage.getItem("email"),
+            password: localStorage.getItem("password")
+        }
+    }).then(async (res) => {
+        console.log(res);
+        if (res.status === 204) {
+            alert("File deleted successfully.");
+        } else {
+            alert("The deletion of the file could not be executed. Please try again.")
+        }
+    }).catch(error=>{
+        console.log(error);
+    });
+
+    axios.get(`${API_URL}submodels/aHR0cHM6Ly9leGFtcGxlLmNvbS9pZHMvc20vMzM4MV80MTYwXzQwMzJfMzc1Mw/submodelelements/roleMapping.roleMapping3.subjects.${accID}`, {
+        auth: {
+            username: localStorage.getItem("email"),
+            password: localStorage.getItem("password")
+        }
+    }).then(async (res) => {
+        console.log(res);
+        if (res.status === 204) {
+            alert("File deleted successfully.");
+        } else {
+            alert("The deletion of the file could not be executed. Please try again.")
+        }
+    }).catch(error=>{
+        console.log(error);
+    });
+
+    // axios.put(`${API_URL}submodels/aHR0cHM6Ly9leGFtcGxlLmNvbS9pZHMvc20vMzM4MV80MTYwXzQwMzJfMzc1Mw/submodelelements/basicAuth/${accID}`, {
+    //     auth: {
+    //         username: localStorage.getItem("email"),
+    //         password: localStorage.getItem("password")
+    //     }
+    // }).then(async (res) => {
+    //     console.log(res);
+    //     if (res.status === 204) {
+    //         alert("File deleted successfully.");
+    //     } else {
+    //         alert("The deletion of the file could not be executed. Please try again.")
+    //     }
+    // }).catch(error=>{
+    //     console.log(error);
+    // });
+}
+
+const PopUpEditAccount = ({ showModal, handleClose, accID }) => {
+    return (
+        <Modal show={showModal} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Delete</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Text className="text">
+                        Are you sure you want to delete this Account?
+                    </Form.Text>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="btn btn-primary btn-sm" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="outline-danger btn-sm" onClick={handleDelete(accID)}>
+                    Delete
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default PopUpEditAccount;
